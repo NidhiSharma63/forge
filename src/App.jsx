ab1a18a67f05a4ed076dda9eef8ec26dd7301e61
NidhiSharma63 <nidhisharma639593@gmail.com>
2025-07-10 23:25:17 +0530

    dummy data stored and displayed

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,55 @@
+import { useState } from "react";
 const App = () => {
   let draggedElement = null;
+  const [data, setData] = useState([
+    {
+      id: "root",
+      children: [],
+    },
+  ]);
 
   const handleDragStart = (event) => {
     draggedElement = event.target;
   };
+
   const handleDrop = (event) => {
     event.preventDefault();
-
     const draggedElementDropped = event.currentTarget; // get where the element is dropped
-
-    // append the element in root
-    // first clone the element the append coz it directly append the original element not the copy
-    const clone = draggedElement.cloneNode(true);
-    draggedElementDropped.appendChild(clone);
-    // if dragged element type is div then make it full height and width and also add some height to it
-    clone.style.width = "100%";
-    clone.style.height = "50px";
-    clone.setAttribute("draggable", false); 
-
-    console.log("Dropped:", draggedElement, "on", draggedElementDropped);
+    setData((prev) =>
+      prev.map((item) =>
+        item.id === "root"
+          ? {
+              ...item,
+              children: [
+                ...item.children,
+                {
+                  id: Math.random(),
+                  type: draggedElement.tagName.toLowerCase(),
+                  props: { text: draggedElement.innerHTML },
+                  children: [],
+                  style: {
+                    height:
+                      draggedElement.tagName.toLowerCase() === "div"
+                        ? "40px"
+                        : draggedElement.getBoundingClientRect().height,
+                    width:
+                      draggedElement.tagName.toLowerCase() === "div"
+                        ? "100%"
+                        : draggedElement.getBoundingClientRect().width,
+                  },
+                },
+              ],
+            }
+          : item
+      )
+    );
+    // console.log("Dropped:", draggedElement, "on", draggedElementDropped);
   };
 
   const handleDragOver = (event) => {
     event.preventDefault(); // Needed to allow drop
   };
+  // console.log(data);
 
   return (
     <div className="mt-3.5">
@@ -39,7 +65,33 @@ const App = () => {
         onDragOver={handleDragOver}
         id="root"
         className="canvas w-[800px] h-[400px] border-2 border-amber-600 m-auto mt-3.5"
-      />
+      >
+        {data
+          .find((item) => item.id === "root")
+          ?.children.map((child) => {
+            const Tag = child.type; // like 'div', 'p', 'button'
+            return (
+              <Tag
+                key={child.id}
+                style={{
+                  width:
+                    typeof child.style.width === "number"
+                      ? `${child.style.width}px`
+                      : child.style.width,
+                  height:
+                    typeof child.style.height === "number"
+                      ? `${child.style.height}px`
+                      : child.style.height,
+                  border: "1px dashed #ccc",
+                  margin: "5px",
+                }}
+                className="p-2"
+              >
+                {child.props.text}
+              </Tag>
+            );
+          })}
+      </div>
     </div>
   );
 };
