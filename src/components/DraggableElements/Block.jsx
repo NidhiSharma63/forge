d097875f900ee69bcac93324bd05eb86890bf0f5
NidhiSharma63 <nidhisharma639593@gmail.com>
2025-07-13 13:49:54 +0530

    block initial props defined

--- src/components/DraggableElements/Block.jsx
+++ src/components/DraggableElements/Block.jsx
@@ -2,13 +2,40 @@ import { useNode } from "@craftjs/core";
 const Block = () => {
   const {
     connectors: { connect, drag },
-  } = useNode();
-  return <div ref={(ref) => connect(drag(ref))}>Block</div>;
+    // Get props from Craft node
+    height,
+    border,
+    borderRadius,
+    width,
+  } = useNode((node) => ({
+    // read here all props properties and can be edit later using setting panel
+    height: node.data.props.height,
+    border: node.data.props.border,
+    width: node.data.props.width || "100%",
+    borderRadius: node.data.props.borderRadius,
+  }));
+  return (
+    <div
+      ref={(ref) => connect(drag(ref))}
+      style={{
+        height,
+        border,
+        width,
+        borderRadius,
+      }}
+    />
+  );
 };
 
-// ✅ This tells Craft.js it’s a valid component
 Block.craft = {
-  displayName: "Block",
+  displayName: "Block", // This tells Craft.js it’s a valid component
+  props: {
+    // Providing defaults props
+    height: "100px",
+    border: "1px solid black",
+    width: "100%",
+    borderRadius: "2px",
+  },
 };
 
 export default Block;
