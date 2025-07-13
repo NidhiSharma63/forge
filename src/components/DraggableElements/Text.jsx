270f45bfdc8d4a75f24c94d2f2c8a9865826fd8a
NidhiSharma63 <nidhisharma639593@gmail.com>
2025-07-13 14:04:53 +0530

    Text initial props fixed

--- src/components/DraggableElements/Text.jsx
+++ src/components/DraggableElements/Text.jsx
@@ -4,24 +4,45 @@ import ContentEditable from "react-contenteditable";
 export const Text = ({ text = "Text" }) => {
   const {
     connectors: { connect, drag },
-    actions: { setProp },
-  } = useNode();
-
+    padding,
+    border,
+    borderRadius,
+    width,
+  } = useNode((node) => ({
+    // read here all props properties and can be edit later using setting panel
+    padding: node.data.props.padding,
+    border: node.data.props.border,
+    borderRadius: node.data.props.borderRadius,
+    width: node.data.props.width,
+  }));
   return (
-    <div ref={(ref) => connect(drag(ref))}>
+    <div
+      style={{
+        padding,
+        border,
+        borderRadius,
+        width,
+      }}
+      ref={(ref) => connect(drag(ref))}
+    >
       <ContentEditable
         html={text}
-        onChange={(e) =>
-          setProp(
-            (props) =>
-              (props.text = e.target.value.replace(/<\/?[^>]+(>|$)/g, ""))
-          )
-        }
+        // onChange={(e) =>
+        //   setProp(
+        //     (props) =>
+        //       (props.text = e.target.value.replace(/<\/?[^>]+(>|$)/g, ""))
+        //   )
+        // }
         tagName="p"
       />
     </div>
   );
 };
 
-Text.craft = { displayName: "Text" };
+Text.craft = {
+  displayName: "Text",
+  props: {
+    width: "fit-content",
+  },
+};
 export default Text;
